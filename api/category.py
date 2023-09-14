b05f6529a655e7a4ce67b2d02d4623c752306123
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2023-09-14 15:11:58 +0700

    category api done but not tested

---
+++ api/category.py
@@ -0,0 +1,251 @@
+from flask import (
+    request, Blueprint, current_app, make_response, jsonify
+    )
+from datetime import datetime
+
+import psycopg2
+
+from .database import get_db_connection
+from . import util
+from .auth import token_required
+
+bp = Blueprint("category", __name__)
+
+def categoryJson(item):
+    return {
+        "category_code": item["category_code"],
+        "category_name": item["category_name"],
+        "category_note": item["category_note"]
+    }
+
+@bp.route("/categorys", methods=["POST"])
+@token_required
+def categorys():
+    if(request.method == "POST"):
+        try:
+            conn = get_db_connection()
+            cur = conn.cursor()
+            content = request.get_json()
+
+            error = ""
+            if(not('category_name' in content.keys()) or len(content['category_name']) == 0):
+                error+="Nama Batasan Kosong! "
+            
+            category_note = ""
+            if('category_note' in content.keys()):
+                category_note = content["category_note"]
+
+            if(len(error) > 0):
+                return util.log_response({
+                    "success": False,
+                    "message": "Data tidak lengkap! " + error,
+                }, 400, request.method)
+
+            cur.execute("""
+                SELECT
+                    CONCAT('C',TO_CHAR(CURRENT_DATE,'YYMM'),LPAD(CAST(COALESCE(CAST(MAX(RIGHT(category_code,4)) AS INT)+1,1) AS VARCHAR),4,'0')) as category_code
+                FROM
+                    m_category
+                WHERE
+                    category_code LIKE CONCAT('C',TO_CHAR(CURRENT_DATE,'YYMM'),'%')
+                """)
+            data = cur.fetchone()
+            category_code = data["category_code"]
+            
+            cur.execute("""
+                INSERT INTO
+                    m_category
+                VALUES
+                    (%s, %s, %s, %s, %s, %s, %s, %s)
+            """, (
+                category_code, 
+                content['category_name'], 
+                category_note, 
+                'Y', 
+                current_app.config['USER_CODE'], 
+                datetime.now(), 
+                current_app.config['USER_CODE'], 
+                datetime.now())
+            )
+            conn.commit()
+            cur.close()
+            conn.close()
+            return util.log_response({
+                "success": True,
+                "message": "Data sudah dimasukkan"
+            }, 200, request.method)
+        except psycopg2.Error as error:
+            return util.log_response({
+                "success": False,
+                "message": error.pgerror,
+            }, 400, request.method)
+
+@bp.route("/category/<category_code>", methods=["GET", "PUT", "DELETE"])
+@token_required
+def category(category_code):
+    if(request.method == "GET"):
+        try:
+            conn = get_db_connection()
+            cur = conn.cursor()
+
+            cur.execute("""
+                SELECT *
+                FROM m_category
+                WHERE category_code = %s
+            """, (category_code,))
+
+            data = cur.fetchone()
+            return make_response(jsonify(categoryJson(data)))
+        except psycopg2.Error as error:
+            return make_response(jsonify({
+                "success": False,
+                "message": error.pgerror,
+            }), 400)
+
+    elif request.method == "PUT":
+        try:
+            conn = get_db_connection()
+            cur = conn.cursor()
+            content = request.get_json()
+
+            error = ""
+            if(not('category_name' in content.keys()) or len(content['category_name']) == 0):
+                error+="Nama Batasan Kosong! "
+            
+            category_note = ""
+            if('category_note' in content.keys()):
+                category_note = content["category_note"]
+
+            if(len(error) > 0):
+                return util.log_response({
+                    "success": False,
+                    "message": "Data tidak lengkap! " + error,
+                }, 400, request.method)
+
+            cur.execute("""
+                UPDATE
+                    m_category
+                SET
+                    category_name = %s,
+                    category_note = %s,
+                    is_active = %s,
+                    update_by = %s,
+                    update_date = %s
+                WHERE 
+                    category_code = %s
+            """, (
+                content['category_name'], 
+                category_note, 
+                'Y', 
+                current_app.config['USER_CODE'], 
+                datetime.now(), 
+                category_code,))
+            conn.commit()
+
+            dataUpdated = cur.fetchone()
+            cur.close()
+            conn.close()
+            if(dataUpdated == None):
+                return util.log_response({
+                    "success": False,
+                    "message": "Data tidak ditemukan"
+                }, 200, request.method) 
+
+            return util.log_response({
+                "success": True,
+                "message": "Data sudah diupate"
+            }, 200, request.method)
+        except psycopg2.Error as error:
+            return make_response(jsonify({
+                "success": False,
+                "message": error.pgerror,
+            }), 400)
+
+    elif request.method == "DELETE":
+        try:
+            conn = get_db_connection()
+            cur = conn.cursor()
+
+            cur.execute("""
+                DELETE FROM
+                    m_category
+                WHERE 
+                    category_code = %s
+                RETURNING *
+            """, (category_code,))
+            conn.commit()
+
+            dataDeleted = cur.fetchone()
+            cur.close()
+            conn.close()
+            if(dataDeleted == None):
+                return util.log_response({
+                    "success": False,
+                    "message": "Data tidak ditemukan"
+                }, 200, request.method) 
+
+            return util.log_response({
+                "success": True,
+                "message": "Data sudah didelete"
+            }, 200, request.method) 
+        except psycopg2.Error as error:
+            return make_response(jsonify({
+                "success": False,
+                "message": error.pgerror,
+            }), 400)
+
+@bp.route("/pagination_category", methods=["POST"])
+@token_required
+def pagination_category():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        content = request.get_json()
+
+        filter= util.filter(content)
+
+        if(filter==''):
+            sql = """
+                SELECT
+                    *
+                FROM
+                    m_category
+                """ + util.sort(content) + """
+                LIMIT
+                    """ + str(content['limit']) + """
+                OFFSET
+                    """ + str(int(content['limit']) * (int(content['page']) - 1)) + """
+                """
+        else:
+            sql = """
+                SELECT
+                    *
+                FROM
+                    m_category
+                WHERE
+                    (""" + util.filter(content) + """) """ + util.sort(content) + """
+                LIMIT
+                    """ + str(content['limit']) + """
+                OFFSET
+                    """ + str(int(content['limit']) * (int(content['page']) - 1)) + """
+                """
+        cur.execute(sql)
+        datas = cur.fetchall()
+        cur.close()
+        conn.close()
+        
+        categorys = []
+        for data in datas:
+            categorys.append(categoryJson(data))
+
+        return util.log_response(
+        {
+            "data": categorys,
+            "message": "success"
+        }, 
+        200, request.method)
+    except psycopg2.Error as error:
+        return make_response(jsonify({
+            "success": False,
+            "message": error.pgerror,
+        }), 400)
