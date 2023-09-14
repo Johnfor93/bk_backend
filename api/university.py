2cb0e7d0c0cfc9aca4ac46d74695a3c3f3bd8572
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2023-09-14 15:30:02 +0700

    university api done but not tested

---
+++ api/university.py
@@ -0,0 +1,251 @@
+from flask import (
+    request, Blueprint, current_app, make_response, jsonify
+    )
+from datetime import datetime
+
+import psycopg2
+
+from .database import get_db_connection
+from . import util
+from .auth import token_required
+
+bp = Blueprint("university", __name__)
+
+def universityJson(item):
+    return {
+        "university_code": item["university_code"],
+        "university_name": item["university_name"],
+        "university_note": item["university_note"]
+    }
+
+@bp.route("/universitys", methods=["POST"])
+@token_required
+def universitys():
+    if(request.method == "POST"):
+        try:
+            conn = get_db_connection()
+            cur = conn.cursor()
+            content = request.get_json()
+
+            error = ""
+            if(not('university_name' in content.keys()) or len(content['university_name']) == 0):
+                error+="Nama Universitas Kosong! "
+            
+            university_note = ""
+            if('university_note' in content.keys()):
+                university_note = content["university_note"]
+
+            if(len(error) > 0):
+                return util.log_response({
+                    "success": False,
+                    "message": "Data tidak lengkap! " + error,
+                }, 400, request.method)
+
+            cur.execute("""
+                SELECT
+                    CONCAT('UNV',TO_CHAR(CURRENT_DATE,'YYMM'),LPAD(CAST(COALESCE(CAST(MAX(RIGHT(university_code,4)) AS INT)+1,1) AS VARCHAR),4,'0')) as university_code
+                FROM
+                    m_university
+                WHERE
+                    university_code LIKE CONCAT('UNV',TO_CHAR(CURRENT_DATE,'YYMM'),'%')
+                """)
+            data = cur.fetchone()
+            university_code = data["university_code"]
+            
+            cur.execute("""
+                INSERT INTO
+                    m_university
+                VALUES
+                    (%s, %s, %s, %s, %s, %s, %s, %s)
+            """, (
+                university_code, 
+                content['university_name'], 
+                university_note, 
+                'Y', 
+                current_app.config['USER_CODE'], 
+                datetime.now(), 
+                current_app.config['USER_CODE'], 
+                datetime.now())
+            )
+            conn.commit()
+            cur.close()
+            conn.close()
+            return util.log_response({
+                "success": True,
+                "message": "Data sudah dimasukkan"
+            }, 200, request.method)
+        except psycopg2.Error as error:
+            return util.log_response({
+                "success": False,
+                "message": error.pgerror,
+            }, 400, request.method)
+
+@bp.route("/university/<university_code>", methods=["GET", "PUT", "DELETE"])
+@token_required
+def university(university_code):
+    if(request.method == "GET"):
+        try:
+            conn = get_db_connection()
+            cur = conn.cursor()
+
+            cur.execute("""
+                SELECT *
+                FROM m_university
+                WHERE university_code = %s
+            """, (university_code,))
+
+            data = cur.fetchone()
+            return make_response(jsonify(universityJson(data)))
+        except psycopg2.Error as error:
+            return make_response(jsonify({
+                "success": False,
+                "message": error.pgerror,
+            }), 400)
+
+    elif request.method == "PUT":
+        try:
+            conn = get_db_connection()
+            cur = conn.cursor()
+            content = request.get_json()
+
+            error = ""
+            if(not('university_name' in content.keys()) or len(content['university_name']) == 0):
+                error+="Nama Universitas Kosong! "
+            
+            university_note = ""
+            if('university_note' in content.keys()):
+                university_note = content["university_note"]
+
+            if(len(error) > 0):
+                return util.log_response({
+                    "success": False,
+                    "message": "Data tidak lengkap! " + error,
+                }, 400, request.method)
+
+            cur.execute("""
+                UPDATE
+                    m_university
+                SET
+                    university_name = %s,
+                    university_note = %s,
+                    is_active = %s,
+                    update_by = %s,
+                    update_date = %s
+                WHERE 
+                    university_code = %s
+            """, (
+                content['university_name'], 
+                university_note, 
+                'Y', 
+                current_app.config['USER_CODE'], 
+                datetime.now(), 
+                university_code,))
+            conn.commit()
+
+            dataUpdated = cur.fetchone()
+            cur.close()
+            conn.close()
+            if(dataUpdated == None):
+                return util.log_response({
+                    "success": False,
+                    "message": "Data tidak ditemukan"
+                }, 200, request.method) 
+
+            return util.log_response({
+                "success": True,
+                "message": "Data sudah diupate"
+            }, 200, request.method)
+        except psycopg2.Error as error:
+            return make_response(jsonify({
+                "success": False,
+                "message": error.pgerror,
+            }), 400)
+
+    elif request.method == "DELETE":
+        try:
+            conn = get_db_connection()
+            cur = conn.cursor()
+
+            cur.execute("""
+                DELETE FROM
+                    m_university
+                WHERE 
+                    university_code = %s
+                RETURNING *
+            """, (university_code,))
+            conn.commit()
+
+            dataDeleted = cur.fetchone()
+            cur.close()
+            conn.close()
+            if(dataDeleted == None):
+                return util.log_response({
+                    "success": False,
+                    "message": "Data tidak ditemukan"
+                }, 200, request.method) 
+
+            return util.log_response({
+                "success": True,
+                "message": "Data sudah didelete"
+            }, 200, request.method) 
+        except psycopg2.Error as error:
+            return make_response(jsonify({
+                "success": False,
+                "message": error.pgerror,
+            }), 400)
+
+@bp.route("/pagination_university", methods=["POST"])
+@token_required
+def pagination_university():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        content = request.get_json()
+
+        filter= util.filter(content)
+
+        if(filter==''):
+            sql = """
+                SELECT
+                    *
+                FROM
+                    m_university
+                """ + util.sort(content) + """
+                LIMIT
+                    """ + str(content['limit']) + """
+                OFFSET
+                    """ + str(int(content['limit']) * (int(content['page']) - 1)) + """
+                """
+        else:
+            sql = """
+                SELECT
+                    *
+                FROM
+                    m_university
+                WHERE
+                    (""" + util.filter(content) + """) """ + util.sort(content) + """
+                LIMIT
+                    """ + str(content['limit']) + """
+                OFFSET
+                    """ + str(int(content['limit']) * (int(content['page']) - 1)) + """
+                """
+        cur.execute(sql)
+        datas = cur.fetchall()
+        cur.close()
+        conn.close()
+        
+        universitys = []
+        for data in datas:
+            universitys.append(universityJson(data))
+
+        return util.log_response(
+        {
+            "data": universitys,
+            "message": "success"
+        }, 
+        200, request.method)
+    except psycopg2.Error as error:
+        return make_response(jsonify({
+            "success": False,
+            "message": error.pgerror,
+        }), 400)
