a05cdd653afbaff6cbc5d6cb05f832d71ac0ac8e
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2023-10-02 11:22:04 +0700

    counseling class report

--- api/counseling.py
+++ api/counseling.py
@@ -47,6 +47,23 @@ def counselingHistoryJson(item):
         "counseling_note"       : item["counseling_note"]
     }
 
+def counselingReportJson(item):
+    return {
+        "counseling_code"       : item["counseling_code"],
+        "student_code"          : item["student_code"],
+        "student_name"          : item["student_name"],
+        "scope_code"            : item["scope_code"],
+        "scope_name"            : item["scope_name"],
+        "category_code"         : item["category_code"],
+        "category_name"         : item["category_name"],
+        "employee_code"         : item["employee_code"],
+        "counseling_date"       : item["counseling_date"],
+        "problem"               : item["problem"],
+        "conclusion"            : item["conclusion"],
+        "followup"              : item["followup"],
+        "counseling_note"       : item["counseling_note"]
+    }
+
 def counselingPagingFormatJSON(item, nameStudent):
     return {
         "counseling_code"       : item["counseling_code"],
@@ -636,6 +653,132 @@ def historyStudent(student_code):
 
         return make_response(jsonify({
             "data":dataJSON,
+            "success": True}), 200)
+    except psycopg2.Error as error:
+        return make_response(jsonify({
+            "success": False,
+            "message": error.pgerror,
+        }), 400)
+
+@bp.route("/classreport/counseling/<classroom_code>/<organization_code>")
+@employee_required
+def overviewClassReport(classroom_code, organization_code):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        timenow = datetime.now()
+
+        yearnow = timenow.year
+        monthnow = timenow.month
+
+        if(monthnow >= 7):
+            periodStart = yearnow
+            periodEnd = yearnow+1
+        else:
+            periodStart = yearnow-1
+            periodEnd = yearnow
+
+        periodYear = str(periodStart)+"/"+str(periodEnd)
+
+        dateStartFirst = str(periodStart)+"-07-01"
+        dateEndSecond = str(periodEnd)+"-06-30"
+
+        limit = '1000'
+
+        payload = {
+                "limit": limit,
+                "page": 1,
+                "filters": [
+                    {
+                        "operator": "contains",
+                        "search": "subject_code",
+                        "value1": "bimbingan_konseling"
+                    },
+                    {
+                        "operator": "contains",
+                        "search": "period_code",
+                        "value1": periodYear
+                    },
+                    {
+                        "operator": "contains",
+                        "search": "organization_code",
+                        "value1": organization_code
+                    },
+                    {
+                        "operator": "contains",
+                        "search": "classroom_code",
+                        "value1": classroom_code
+                    }
+                ],
+                "filter_type": "AND"
+            }
+        
+        headers = {
+                'token': request.headers.get('token'),
+                'Content-Type': 'application/json',
+                'Accept': '*',
+                'Proxy-Authorization': 'http://192.168.100.104:7001',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+            }
+        
+        url = ("http://192.168.100.104:7001/employee_education_detail_paging")
+
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
+        
+        if(response.status_code != 200):
+            return util.log_response({
+                "success": False,
+                "message": "Laman tidak dapat diakses",
+            }, 400, request.method)
+            
+        datas = response.json()
+        dataStudent = datas["data"]
+
+        cur.execute("""
+            CREATE TEMP TABLE students(student_code VARCHAR(40), student_name VARCHAR(40))
+        """)
+
+        for item in dataStudent:
+            cur.execute("""
+                INSERT INTO students VALUES (%s, %s)
+            """, (item["student_code"], item["student_name"],))
+
+        cur.execute("""
+            SELECT 
+                counseling_code,
+                students.student_code,
+                students.student_name,
+                m_scope.scope_code,
+                m_scope.scope_name,
+                m_category.category_code,
+                m_category.category_name,
+                employee_code,
+                counseling_date,
+                problem,
+                conclusion,
+                followup,
+                counseling_note
+            FROM 
+                t_counseling
+                INNER JOIN m_scope ON t_counseling.scope_code = m_scope.scope_code
+                INNER JOIN m_category ON t_counseling.category_code = m_category.category_code
+                INNER JOIN students ON t_counseling.student_code = students.student_code
+            WHERE
+                counseling_date BETWEEN %s AND %s
+        """, (dateStartFirst, dateEndSecond,))
+
+        classReportDatas = cur.fetchall()
+        cur.close()
+        conn.close()
+
+        classReportJSON = list()
+
+        for data in classReportDatas():
+            classReportDatas.append(counselingReportJson(data))
+
+        return make_response(jsonify({
+            "data":classReportJSON,
             "success": True}), 200)
     except psycopg2.Error as error:
         return make_response(jsonify({
